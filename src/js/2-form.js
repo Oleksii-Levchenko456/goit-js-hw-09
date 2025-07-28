69572a1bd16bbed7af18aed1f5abddcbc7726af8
Oleksii <neonator3000@gmail.com>
2025-07-28 20:13:32 +0200

    fix task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,12 +1,11 @@
-import { stringify } from "postcss"
-
 const formData = {
-    email: '',
-    message: ''
+    email: "",
+    message: "" 
 }
 
 const input = document.querySelector('[name="email"]')
 const textarea = document.querySelector('[name="message"]')
+const form = document.querySelector('.feedback-form')
 
 
 
@@ -21,28 +20,38 @@ if (feedback) {
 
 
 
-input.addEventListener('input', event => {
-    formData.email = event.target.value
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
-})
-textarea.addEventListener('input', event => {
-    formData.message = event.target.value
-        localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+// input.addEventListener('input', event => {
+//     formData.email = event.target.value
+//     localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+// })
+// textarea.addEventListener('input', event => {
+//     formData.message = event.target.value
+//         localStorage.setItem("feedback-form-state", JSON.stringify(formData))
 
+// })
+
+
+// Збереження в local.storage
+form.addEventListener('input', event => {
+    const target = event.target
+    if ((target.nodeName === 'INPUT' || target.nodeName === 'TEXTAREA') && target.name) {
+        formData[target.name] = target.value
+        localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+    }
 })
 
 // localStorage.setItem("feedback-form-state", formDataJSON)
 // if (formData.email === '' || formData.message === '') {
 //     alert('«Fill please all fields»')
 // }
-const form = document.querySelector('.feedback-form')
+
 form.addEventListener('submit', event => {
      event.preventDefault();
     if (input.value.trim() === '' || textarea.value.trim() === '') {
         alert('«Fill please all fields»')
     } else {
         console.log(formData)
-        localStorage.clear()
+        localStorage.removeItem("feedback-form-state")
         input.value = ''
         textarea.value = ''
         formData.email = '';
